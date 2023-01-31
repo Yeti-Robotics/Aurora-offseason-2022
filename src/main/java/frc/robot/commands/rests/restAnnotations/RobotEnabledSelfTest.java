91827c8ae360a00454c27dd0a29586c937d9e016
vwu1888 <vwu1888@gmail.com>
2023-01-30 23:42:14 -0500

    removed unused annotations

--- src/main/java/frc/robot/commands/rests/restAnnotations/RobotEnabledSelfTest.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.commands.rests.restAnnotations;
-
-import javax.inject.Qualifier;
-import java.lang.annotation.*;
-
-@Qualifier
-@Retention(RetentionPolicy.RUNTIME)
-public @interface RobotEnabledSelfTest {
-}
